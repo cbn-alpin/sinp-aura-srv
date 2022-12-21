84b32c7195d7dc48ebf24660b6aabd95fda62aca
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-12-21 11:05:49 +0100

    feat(web-srv/docker): improve biodiv'territoires compose and build files

---
+++ web-srv/home/admin/docker/biodivterritoires/build/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "+-------------------------------------------+"
+echo "|               GeoNature                   |"
+echo "|        Biodiversité des territoires       |"
+echo "+-------------------------------------------+"
+
+# Set default variables values
+app_name="${app_name:-gnbt}"
+gun_num_workers=${gun_num_workers:-4}
+gun_timeout=${gun_timeout:-30}
+gun_host=${gun_host:-0.0.0.0}
+gun_port=${gun_port:-8080}
+
+until [ "$(psql $(python -c 'print("'${SQLALCHEMY_DATABASE_URI}'".replace("+psycopg2",""))') -XtAc "SELECT 1")" = '1' ]; do
+  echo "Awaiting Database container"
+  sleep 1
+done
+sleep 2
+
+cd /app
+
+echo "DEBUG: ${DEBUG}"
+
+if [ "${DEBUG}" == "true" ] ; then
+  echo "Mode: debug"
+  python -m wsgi
+else
+  echo "Mode: production"
+  echo "Gunicorn config:"
+  echo -e "\t app_name=${app_name}"
+  echo -e "\t gun_num_workers=${gun_num_workers}"
+  echo -e "\t gun_timeout=${gun_timeout}"
+  echo -e "\t gun_host=${gun_host}"
+  echo -e "\t gun_port=${gun_port}"
+
+  gunicorn wsgi:app --error-log - --access-logfile - \
+        --pid="${app_name}.pid" \
+        -w "${gun_num_workers}" \
+        -t "${gun_timeout}" \
+        -b "${gun_host}:${gun_port}" \
+        -n "${app_name}"
+fi
